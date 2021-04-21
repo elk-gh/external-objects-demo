1bf00f3dacab91c3f9dd4e01e01bbad011304cd7
elk-gh <elkin.cordobab@gmail.com>
2021-04-20 22:02:02 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs/promises')
+const port = process.env.PORT || 3000
+
+const server = http.createServer( async (req, res) => {
+    if (req.url === '/')  {
+        res.writeHead(200, {'Content-Type' : 'text/html'})
+        res.write('<h1> Hola Bogota DUG </h1>') 
+    } else if (req.url === 'api/publications'){
+        res.writeHead(200, {'Content-Type' : 'application/json'})
+        const file = await fs.readFile('./publications.json','utf8')
+        res.write(file)
+    }else{
+        res.writeHead(404, {'Content-Type':'text/plain'})
+        res.write(`404: ${req.url} not found`)
+    }
+    res.end()
+})
