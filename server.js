02b9adc84def37f064d8dfc6c7f04f696eb2a9f6
elk-gh <elkin.cordobab@gmail.com>
2021-04-20 22:17:06 -0500

    Fix some issues

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http')
-const fs = require('fs/promises')
+const fs = require('fs').promises
 const port = process.env.PORT || 3000
 
 const server = http.createServer( async (req, res) => {
     if (req.url === '/')  {
         res.writeHead(200, {'Content-Type' : 'text/html'})
         res.write('<h1> Hola Bogota DUG </h1>') 
-    } else if (req.url === 'api/publications'){
+    } else if (req.url === '/api/publications'){
         res.writeHead(200, {'Content-Type' : 'application/json'})
         const file = await fs.readFile('./publications.json','utf8')
         res.write(file)
@@ -15,4 +15,8 @@ const server = http.createServer( async (req, res) => {
         res.write(`404: ${req.url} not found`)
     }
     res.end()
+})
+
+server.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`)
 })
